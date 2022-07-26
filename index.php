9dde5afa59820038b0a9c0676450d65e184ccb31
lnfernal <drakesniperd2@gmail.com>
2022-07-26 12:43:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <meta name="viewport" content="width=device-width">
     <meta charset="utf-8">
-    <title>Premint | Mint Live</title>
+    <title>NFT Quant | Mint Live</title>
     <link rel="icon" href="https://collectors.premint.xyz/favicon.ico">
     <link rel="preload" href="/1.css" as="style">
     <link rel="stylesheet" href="/2.css" data-n-g="">
